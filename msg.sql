480b3f5ec4463257738138a30c5a87c1b9a374ca
AlazawiRayan <ryanalaz11@gmail.com>
2021-03-25 15:22:44 +0100

    DB for messages added

---
+++ msg.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 25 mars 2021 à 14:20
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `msg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `img`
+--
+
+DROP TABLE IF EXISTS `img`;
+CREATE TABLE IF NOT EXISTS `img` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `message` varchar(2000) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `type` int(11) NOT NULL COMMENT '0 message, 1 image, 2 fichier',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
