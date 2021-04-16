9d54d11d1cc1ffb5a9f04576d0efc8f612268b9e
KAABECHE rayane <rayanoo1302@gmail.com>
2021-04-16 10:19:39 +0200

    Update msg.sql

--- msg.sql
+++ msg.sql
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `message_log` (
 )  ENGINE=InnoDB AUTO_INCREMENT=2;
 
 CREATE TABLE IF NOT EXISTS `accounts` (
-   `id` varchar(255) NOT NULL,
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(255),
   `email` varchar(255)
