29d289886fc86dbb5be96dee7390d06cc9d3c7be
AlazawiRayan <ryanalaz11@gmail.com>
2021-04-18 15:42:22 +0200

    Saved

--- public/profile.js
+++ public/profile.js
@@ -1,4 +1,6 @@
-var uname = sessionStorage.getItem("uname");
+const socket = io("ws://localhost:5000");
+getUID();
+
 
 function loadForm() {
     document.getElementById('buttonid').addEventListener('click', openDialog);
@@ -8,26 +10,58 @@ function loadForm() {
     document.getElementById('inputid').addEventListener('change', submitForm);
     function submitForm() {
         if(confirm("Voulez vous vraiment changer votre photo de profil ?")) document.getElementById('autosubmit_form').submit();
-        socket.on("PPUpdated", (url) => {
-            setProfilePictureUrl(url);
-        })
     }
 }
 
+
 function confirmDelete() {
-    if(confirm("Voulez vous vraiment supprimer votre compte ?")) {
-        socket.emit("del-acc", uid);
-    }
+    /*if(confirm("Voulez vous vraiment supprimer votre compte ?")) {
+        //socket.emit("del-acc", uid);
+        
+        alert("Hello");
+    }*/
+}
+
+function getUID() {
+    let t = new Promise(function(resolve, reject) {
+        socket.emit("logged-in", "a", (response) => {
+            if(response != null) resolve(response);
+        })
+        setTimeout(() => {}, 5000);
+    });
+    t.then((res)=>{
+        sessionStorage.setItem("uid", res);
+        console.log(res);
+    }).catch(()=>{
+
+    });
 }
 
 function getProfilePictureUrl() {
-    socket.emit("sql-select", `SELECT 'profile_picture' FROM 'accounts' WHERE 'username' = ${uname}`, (response) => {
-        return response;
+
+    /*let t = new Promise(function(resolve, reject) {
+        socket.emit("sql-select", `SELECT 'profile_picture' FROM 'accounts' WHERE 'username' = ${uid}`, (response) => {
+            resolve(response);
+        })
+        setTimeout(() => {}, 500);
+    });
+    t.then((res)=>{
+        console.log("res");
+        return res;
+    }).catch(()=>{
+
+    });*/
+    var a = "SELECT profile_picture FROM accounts WHERE id = " + sessionStorage.getItem("uid");
+    console.log(a);
+    socket.emit("sql-select", a, (response) => {
+        return response[0].profile_picture;
     })
+   
 }
 
 function setProfilePictureUrl(url) {
-    socket.emit("sql-update", `UPDATE 'accounts' SET 'profile_picture' = ${url}`, (response) => {
-
+    var a = "UPDATE accounts SET profile_picture = " + url + " WHERE id = " + sessionStorage.getItem("uid");
+    socket.emit("sql-update", a, (response) => {
+        console.log("Done");
     })
 }
