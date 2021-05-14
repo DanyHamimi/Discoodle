9f7cb26ec5973ff509223da88b0b7ef068a50b3f
Kaabeche <you@example.com>
2021-05-15 00:24:22 +0200

    msg_error_correction

--- public/script.js
+++ public/script.js
@@ -3,6 +3,8 @@ console.log(defaultId);
 (function($) {
     "use strict"; // Start of use strict
 
+
+    var socket = io();
     function showGroup(id) {
         defaultId = id;
         if (typeof id == 'undefined') {
@@ -23,10 +25,19 @@ console.log(defaultId);
         showGroup(defaultId);
     });
 
+
     $('.trigger-group').click(function() {
         console.log("here");
         console.log($(this).attr('data'));
         var id = $(this).attr('data');
         showGroup(id);
     });
+
+    $(document).on('click', '.trigger-group', function(){ 
+        console.log("here");
+        console.log($(this).attr('data'));
+        var id = $(this).attr('data');
+        showGroup(id);
+       
+   });
 })(jQuery); // End of use strict
